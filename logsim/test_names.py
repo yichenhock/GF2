02c3220e54ce8d3f559fd27ad81052bf271b9821
Michael Stevens <michael246@hotmail.co.uk>
2022-05-27 15:47:23 +0100

    Finished test_names.py including PEP 8/257 convention changes

--- logsim/test_names.py
+++ logsim/test_names.py
@@ -6,23 +6,26 @@ from names import Names
 
 @pytest.fixture
 def new_names():
-    """Return a new instance of the Names class"""
+    """Return a new instance of the Names class."""
     return Names()
+
 
 @pytest.fixture
 def name_string_list():
-    """Return a list of example names"""
+    """Return a list of example names."""
     return ["cycle", "NAND", "connected"]
+
 
 @pytest.fixture
 def names_with_items(name_string_list):
-    """Return a Names class instance with three items in the names list"""
+    """Return a Names class instance with three items in the names list."""
     new_names = Names()
     [cycle_id, NAND_id, connected_id] = new_names.lookup(name_string_list)
     return new_names
 
+
 def test_unique_error_codes_raises_exceptions(names_with_items):
-    """Test if unique_error_codes raises expected exceptions"""
+    """Test if unique_error_codes raises expected exceptions."""
     with pytest.raises(TypeError):
         names_with_items.unique_error_codes(2.6)
     with pytest.raises(TypeError):
@@ -32,15 +35,17 @@ def test_unique_error_codes_raises_exceptions(names_with_items):
     with pytest.raises(ValueError):
         names_with_items.unique_error_codes(0)
 
+
 def test_query_raises_exceptions(names_with_items):
-    """Test if query raises expected exceptions"""
+    """Test if query raises expected exceptions."""
     with pytest.raises(TypeError):
         names_with_items.query(5)
     with pytest.raises(TypeError):
         names_with_items.query(True)
 
+
 def test_lookup_raises_exceptions(names_with_items):
-    """Test if lookup raises expected exceptions"""
+    """Test if lookup raises expected exceptions."""
     with pytest.raises(TypeError):
         names_with_items.lookup(10)
     with pytest.raises(TypeError):
@@ -50,8 +55,9 @@ def test_lookup_raises_exceptions(names_with_items):
     with pytest.raises(TypeError):
         names_with_items.lookup(["test 3", 10.2, "test4"])
 
+
 def test_get_name_string_raises_exceptions(names_with_items):
-    """Test if get_name_string raises expected exceptions"""
+    """Test if get_name_string raises expected exceptions."""
     with pytest.raises(TypeError):
         names_with_items.get_name_string("test5")
     with pytest.raises(TypeError):
@@ -59,12 +65,14 @@ def test_get_name_string_raises_exceptions(names_with_items):
     with pytest.raises(ValueError):
         names_with_items.get_name_string(-7)
 
+
 def test_unique_error_codes(new_names):
-    """Test if unique_error_codes returns the expected range and changes the error_code_count correctly"""
+    """Test if unique_error_codes returns correct range and counts errors."""
     assert new_names.error_code_count == 0
     assert new_names.unique_error_codes(5) == range(0, 5)
     assert new_names.error_code_count == 5
     assert new_names.unique_error_codes(1) == range(5, 6)
+
 
 @pytest.mark.parametrize("name_string, expected_id", [
     ("cycle", 0),
@@ -72,27 +80,38 @@ def test_unique_error_codes(new_names):
     ("connected", 2),
     ("undefined", None)
 ])
-def test_query(names_with_items, new_names, name_string, expected_id):
-    """Test if query returns the expected id"""
-    #Name is present
+def test_query(new_names, names_with_items, name_string, expected_id):
+    """Test if query returns the expected id."""
+    # Name is present
     assert names_with_items.query(name_string) == expected_id
-    #Name is absent
+    # Name is absent
     assert new_names.query(name_string) is None
 
-def test_lookup(names_with_items):
-    """Test if lookup returns the expected list of name IDs and adds new names correctly"""
-    pass
+
+def test_lookup(new_names, names_with_items):
+    """Test if lookup returns list of name IDs and adds new names correctly."""
+    assert new_names.names == []
+    assert names_with_items.names == ["cycle", "NAND", "connected"]
+    assert names_with_items.lookup(["NAND"]) == [1]
+    assert names_with_items.lookup(["connected", "cycle"]) == [2, 0]
+    # Check for repeats
+    assert names_with_items.lookup(["NAND", "cycle", "NAND"]) == [1, 0, 1]
+    # Check new names added correctly
+    names_with_items.lookup(["OR", "HIGH"])
+    assert names_with_items.names == ["cycle", "NAND", "connected", "OR",
+                                      "HIGH"]
+    assert names_with_items.lookup(["connected", "test6", "OR"]) == [2, 5, 3]
 
 
-@pytest.mark.parametrize("name_id, expected_string", [
+@pytest.mark.parametrize("name_id, expected_str", [
     (0, "cycle"),
     (1, "NAND"),
     (2, "connected"),
     (3, None)
 ])
-def test_get_name_string(names_with_items, new_names, name_id, expected_string):
-    """Test if get_name_string returns the expected string"""
-    #Name is present
-    assert names_with_items.get_name_string(name_id) == expected_string
-    #Name is absent
+def test_get_name_string(names_with_items, new_names, name_id, expected_str):
+    """Test if get_name_string returns the expected string."""
+    # Name is present
+    assert names_with_items.get_name_string(name_id) == expected_str
+    # Name is absent
     assert new_names.get_name_string(name_id) is None
