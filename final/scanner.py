ded2e9e3f2ffb89e1ad86f4a31e2173414352cda
Michael Stevens <73239265+ObsidianSock@users.noreply.github.com>
2022-06-09 22:13:54 +0100

    Added example circuit output diagrams

--- final/scanner.py
+++ final/scanner.py
@@ -103,7 +103,7 @@ class Scanner:
         self.lines = self.file.read().splitlines()
         self.file.seek(0)
 
-    def __advance(self):
+    def _advance(self):
         """Read next character from file."""
         self.current_character = self.file.read(1)
         if self.current_character == "\n":
@@ -113,17 +113,17 @@ class Scanner:
             self.current_character_position += 1
         return
 
-    def __skip_spaces(self):
+    def _skip_spaces(self):
         """Skips until non-space character is reached."""
         while self.current_character.isspace():
-            self.__advance()
+            self._advance()
         return
 
-    def __skip_comment(self):
+    def _skip_comment(self):
         """Skips the current comment (Until next semicolon or newline)."""
         while self.current_character not in [";", "\n", ""]:
-            self.__advance()
-        self.__advance()
+            self._advance()
+        self._advance()
         return
 
     def print_error_line(self, line_number, line_position, error_message=""):
@@ -133,32 +133,32 @@ class Scanner:
         print(self.lines[line_number])
         print(" " * (line_position) + "^ Error here")
 
-    def __get_name(self):
+    def _get_name(self):
         """Read and returns the next name (word made up of only letters)."""
         name = ""
         while self.current_character.isalnum():
             name += self.current_character
-            self.__advance()
+            self._advance()
         return name
 
-    def __get_number(self):
+    def _get_number(self):
         """Read and returns the next number."""
         number = ""
         while self.current_character.isdigit():
             number += self.current_character
-            self.__advance()
+            self._advance()
         return int(number)
 
     def get_symbol(self):
         """Translate the next sequence of characters into a symbol."""
         symbol = Symbol()
-        self.__skip_spaces()  # Current character is now not whitespace
+        self._skip_spaces()  # Current character is now not whitespace
 
         symbol.line_number = self.current_line
         symbol.line_position = self.current_character_position
 
         if self.current_character.isalpha():  # Name
-            name_string = self.__get_name()
+            name_string = self._get_name()
             # print(name_string) # For tests
             if name_string in self.keywords_list:
                 symbol.type = self.KEYWORD
@@ -167,42 +167,42 @@ class Scanner:
             [symbol.id] = self.names.lookup([name_string])
 
         elif self.current_character.isdigit():  # Number
-            symbol.id = self.__get_number()
+            symbol.id = self._get_number()
             # print(symbol.id) # For tests
             symbol.type = self.NUMBER
 
         elif self.current_character == ",":  # Punctuation
             symbol.type = self.COMMA
-            self.__advance()
+            self._advance()
 
         elif self.current_character == ".":
             symbol.type = self.DOT
-            self.__advance()
+            self._advance()
 
         elif self.current_character == ";":
             symbol.type = self.SEMICOLON
-            self.__advance()
+            self._advance()
 
         elif self.current_character == "=":
             symbol.type = self.EQUALS
-            self.__advance()
+            self._advance()
 
         elif self.current_character == "(":
             symbol.type = self.OPEN_BRACKET
-            self.__advance()
+            self._advance()
 
         elif self.current_character == ")":
             symbol.type = self.CLOSE_BRACKET
-            self.__advance()
+            self._advance()
 
         elif self.current_character == "":  # End of File
             symbol.type = self.EOF
 
         elif self.current_character == "#":  # Comment Check
-            self.__skip_comment()
+            self._skip_comment()
             symbol = self.get_symbol()
 
         else:  # Not a valid character (symbol.type == None)
-            self.__advance()
+            self._advance()
 
         return symbol
